2b69a2650dea622f5a4e688e50dc7fc451778c18
_jun <gaosir_801@163.com>
2022-09-04 20:11:57 +0800

    feat:修改devServer配置

--- build/webpack.dev.config.js
+++ build/webpack.dev.config.js
@@ -1,31 +1,29 @@
-const webpackMerge = require("webpack-merge");
-const utils = require("./utils")
+const webpackMerge = require('webpack-merge');
+const utils = require('./utils');
 const entryConfig = require('./entry.dev.config');
-const baseWebpackConfig = require("./webpack.base.config")
+const baseWebpackConfig = require('./webpack.base.config');
 
 module.exports = webpackMerge.merge(baseWebpackConfig, {
-    mode: "development",
+    mode: 'development',
     entry: entryConfig.getBuildEntry(),
     output: {
         path: entryConfig.getPrdOutPutPath(),
-        filename: "static/js/[name].js",
-        publicPath: "/"
+        filename: 'static/js/[name].js',
+        publicPath: '/',
     },
     module: {
-        rules: utils.cssLoaders()
+        rules: utils.cssLoaders(),
     },
-    plugins: [
-        ...entryConfig.getHtmlWebpackPluginList()
-    ],
+    plugins: [...entryConfig.getHtmlWebpackPluginList()],
     devtool: '#eval-source-map',
     devServer: {
-        historyApiFallback: true,
+        port: '8080',
         hot: true,
-        compress: true,
-        port: "8080",
+        open: true,
+        compress: false,
+        historyApiFallback: true,
         proxy: {
             // 接口请求代理
         },
-
-    }
+    },
 });
